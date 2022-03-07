02d4aebe61cb882ec6946e08015a4aa1da2fdea1
Okan <42724589+dovenokan@users.noreply.github.com>
2022-03-07 20:01:32 +0300

    removing some folders

--- old_versions/db_operator.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('wordlisterdb.db')
-print("Opened database successfully")
-
-conn.execute('CREATE TABLE userlist (id integer primary key autoincrement, username TEXT, email TEXT, password TEXT)')
-print("Table created successfully")
-
-conn.close()
